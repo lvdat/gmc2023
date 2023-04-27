8c92ee14239d8364fdc8fe31c57786a521846f1e
lvdat <lvdat13@gmail.comwq>
2023-04-27 01:38:50 +0000

    init login form

--- views/login.php
+++ views/login.php
@@ -2,17 +2,22 @@
     <div class="col-md-4">
         <div class="card">
             <div class="card-body">
-                <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingInput" autofocus="true" required />
-                    <label for="floatingInput">Mã số sinh viên</label>
-                </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="floatingPassword" required />
-                    <label for="floatingPassword">Mật khẩu</label>
-                </div>
-                <div class="d-grid gap-2">
-                    <button class="btn btn-lg btn-dark" type="button">Đăng nhập</button>
-                </div>
+                <form method="post">
+                    <p class="text-center">
+                        Đăng nhập tài khoản GMC2023!
+                    </p>
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" autofocus="true" required />
+                        <label for="floatingInput">Mã số sinh viên</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control" id="floatingPassword" required />
+                        <label for="floatingPassword">Mật khẩu</label>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-lg btn-dark" type="submit">Đăng nhập</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
